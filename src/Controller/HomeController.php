e9ce0a3ecb8008512225c38bbc98bb6bec723342
qtatv2 <137097794+qtatv2@users.noreply.github.com>
2025-11-30 12:28:35 +0100

    packages install

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,11 +6,12 @@ use App\Repository\VacationRequestRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class HomeController extends AbstractController
 {
     #[Route('/api/home', name: 'home', methods: ['GET'])]
-    public function index(VacationRequestRepository $repository): Response
+    public function index(VacationRequestRepository $repository): JsonResponse
     {
         $requests = $repository->findAll();
         //dd($requests);
