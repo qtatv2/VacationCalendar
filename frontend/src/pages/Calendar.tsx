50b13b2576e94b5811efb1782467fc2496f9f9ac
Igor <137097794+qtatv2@users.noreply.github.com>
2026-01-04 21:29:43 +0100

    year and months logic

--- frontend/src/pages/Calendar.tsx
+++ frontend/src/pages/Calendar.tsx
@@ -15,7 +15,15 @@ export default function Calendar() {
   const daysInMonth: number = new Date(currentYear, currentMonth + 1, 0).getDate();
   const startDay: number = (new Date(currentYear, currentMonth, 1).getDay() + 6) % 7;
 
+
   const days: number[] = Array.from({ length: daysInMonth }, (_, i) => i + 1);
+
+
+    const changeMonth = (offset: number) =>{
+      const newDate = new Date(currentYear, currentMonth + offset, 1);
+      setCurrentMonth(newDate.getMonth());
+      setCurrentYear(newDate.getFullYear());
+    }
 
   return <>
     <div className="p-5">
@@ -24,8 +32,12 @@ export default function Calendar() {
         <div className="flex justify-between">
           <h1 className="text-4xl font-bold text-red-400 mb-8">{monthsOfYear[currentMonth]}</h1>
           <div className="flex gap-2">
-            <div className="bg-gray-500 rounded w-12 h-12"><button className="w-full h-full">◀️</button></div>
-            <div className="bg-gray-500 rounded w-12 h-12"><button className="w-full h-full">▶️</button></div>
+            <div className="bg-gray-500 rounded w-12 h-12">
+              <button className="w-full h-full" onClick={()=>changeMonth(-1)}>◀️</button>
+              </div>
+            <div className="bg-gray-500 rounded w-12 h-12">
+              <button className="w-full h-full" onClick={()=>changeMonth(1)}>▶️</button>
+            </div>
           </div>
       </div>
       </div>
@@ -34,6 +46,9 @@ export default function Calendar() {
           {dayOfWeek.map((day, index)=>(
             <div key={index} className="text-2xl font-bold text-yellow-200">{day}</div>
             ))}
+            {Array.from({ length: startDay }, (_, index) =>(
+              <div key={index} className="p-6 invisible"></div>
+          ))}
           {days.map((day, index)=>(
           <Day key={index} dayNumber={index+1}></Day>
           ))}
